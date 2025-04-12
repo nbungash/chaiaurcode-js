6a3f69ef13fd89d7113abcb6e2ec02019fc9f25c
Umer Iqbal <105549278+nbungash@users.noreply.github.com>
2025-04-12 12:32:25 +0000

    conversion of data types

---
+++ 01_basics/conversion_of_datatypes.js
@@ -0,0 +1,11 @@
+// let value = "30abc"      // value: NaN   type: number
+//let value = "abc"         // value: NaN   type: number
+//let value =null           // value: 0     type: number
+// let value = undefined    // value: NaN   type: number
+let value = true            // value: 1     type: number
+
+convertedValue = Number(value)
+console.log(convertedValue)
+console.log(typeof convertedValue)
+
+
